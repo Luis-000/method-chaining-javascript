43fec31575aa991f09ed081fd7021c1391f20825
Luis Costa <129795162+Luis-000@users.noreply.github.com>
2026-06-01 18:46:57 +0100

    Add files via upload

---
+++ Method Chaining/index.js
@@ -0,0 +1,32 @@
+
+
+let username = window.prompt("Enter your username");
+
+
+// NO Method Chaining.
+
+/**
+
+username = username.trim();
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
+
+let extra = username.slice(1);
+extra = extra.toLowerCase();
+
+username = letter + extra;
+
+console.log(username); 
+
+*/
+
+//Method Chaining
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+
+if(username){
+    console.log(username);
+}
+else{
+    console.log(`No username Inserted!`);
+}
